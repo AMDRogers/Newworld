1cd52cce38c8619ed3e3c1e085e40cc68aea09e7
Alex Rogers <Alex.Rogers@Jaama.co.uk>
2025-09-16 09:56:36 +0100

    Fixed reference

--- newworld/src/main.tsx
+++ newworld/src/main.tsx
@@ -5,33 +5,46 @@ import App from './App'
 
 import { PublicClientApplication } from '@azure/msal-browser';
 import { MsalProvider } from '@azure/msal-react';
-import { msalConfig } from './authConfig';  //loginRequest
+import { msalConfig, loginRequest } from './authConfig';  //loginRequest
+
+import { SafeFetch, POST } from './helpers/fetch';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient;
 
+const msalInstance = new PublicClientApplication(msalConfig);
+
 
 
 async function bootstrap() {
-    //await msalInstance.initialize();
+    await msalInstance.initialize();
 
 
-    //try {
-    //    const response = await msalInstance.handleRedirectPromise();
-    //    if (response && response.account) {
-    //        msalInstance.setActiveAccount(response.account);
-    //    } else {
-    //        const accounts = msalInstance.getAllAccounts();
-    //        if (accounts.length > 0) {
-    //            msalInstance.setActiveAccount(accounts[0]);
-    //        } else {
-    //            // Uncomment if you want automatic login on each site
-    //            await msalInstance.loginRedirect(loginRequest);
-    //        }
-    //    }
-    //} catch (error) {
-    //    console.error("MSAL redirect error:", error);
-    //}
+    try {
+        const response = await msalInstance.handleRedirectPromise();
+        if (response && response.account) {
+            msalInstance.setActiveAccount(response.account);
+
+            if (response) {
+                const accessToken = response.accessToken;
+                await SafeFetch("api/StoreToken", POST({ Token: accessToken }));
+            }
+        } else {
+
+            const accounts = msalInstance.getAllAccounts();
+            if (accounts.length > 0) {
+                msalInstance.setActiveAccount(accounts[0]);
+            } else {
+
+                // Uncomment if you want automatic login on each site
+                await msalInstance.loginRedirect(loginRequest);
+
+            }
+        }
+    } catch (error) {
+        console.error("MSAL redirect error:", error);
+    }
+
 
 
     createRoot(document.getElementById('root')!).render(
